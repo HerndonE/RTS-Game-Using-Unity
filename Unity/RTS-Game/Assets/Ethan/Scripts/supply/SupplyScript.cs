808b33c0d28bb483956dc65ac0f646b056b4454d
HerndonE <EHerndon17@gmail.com>
2020-11-08 17:21:23 -0800

    [update] supply functionality

--- Unity/RTS-Game/Assets/Ethan/Scripts/supply/SupplyScript.cs
+++ Unity/RTS-Game/Assets/Ethan/Scripts/supply/SupplyScript.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+﻿/*
+ * Name: Ethan Herndon
+ * Date: 11/8/2020
+ * Class: CST 426
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SupplyScript : MonoBehaviour
 {
+    //TODO: CHECK IF SUPPLY IS DESTROYED THEN REMOVE FROM LIST
 
     public GameObject thePrefab;
-
-   
-
-
+    public int supplyCount;
+  
     void Start()
     {
        
@@ -18,17 +22,12 @@ public class SupplyScript : MonoBehaviour
     void Update()
     {
 
-      
-
-
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 wordPos;
             Ray ray = Camera.main.ScreenPointToRay(mousePos);
             RaycastHit hit;
-
-          
 
             if (Physics.Raycast(ray, out hit, 1000f))
             {
@@ -44,11 +43,10 @@ public class SupplyScript : MonoBehaviour
                 wordPos = Camera.main.ScreenToWorldPoint(mousePos);
             }
             Instantiate(thePrefab, wordPos + Vector3.up * 1f, Quaternion.identity);
-            //or for tandom rotarion use Quaternion.LookRotation(Random.insideUnitSphere)
+            supplyCount += 10;
+            Debug.Log("Supply Count:" + supplyCount);
         }
+       
     }
-
-    
-
 
 }
---
+++ Unity/RTS-Game/Assets/Ethan/Scripts/supply/supplyManager.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Name: Ethan Herndon
+ * Date: 11/8/2020
+ * Class: CST 426
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class supplyManager : MonoBehaviour
+{
+
+    public int numOfSupplyBuildings;
+    public GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GameObject.FindGameObjectsWithTag("supply").Length >= numOfSupplyBuildings)
+        {
+            mainCamera.GetComponent<SupplyScript>().enabled = false;
+        }
+        else
+            mainCamera.GetComponent<SupplyScript>().enabled = true;
+    }
+}
