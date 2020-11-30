139eff7e9382766cabf831c357cc1ed2b54114e7
Xenvex <matthewbreyes@gmail.com>
2020-11-30 15:22:14 -0800

    Test

--- Unity/RTS-Game/Assets/Matthew/Assets/Scripts/Ally.cs
+++ Unity/RTS-Game/Assets/Matthew/Assets/Scripts/Ally.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Ally : MonoBehaviour
 {
-    private Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
