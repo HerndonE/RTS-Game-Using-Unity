f0e4ff2bfe84f8954e351889163cbbc8e1c4224a
Xenvex <matthewbreyes@gmail.com>
2020-11-30 16:37:23 -0800

    Updating Ally and Enemy Script

--- Unity/RTS-Game/Assets/Matthew/Assets/Scripts/Ally.cs
+++ Unity/RTS-Game/Assets/Matthew/Assets/Scripts/Ally.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Ally : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector3(10, 0, 0);
     }
 }
