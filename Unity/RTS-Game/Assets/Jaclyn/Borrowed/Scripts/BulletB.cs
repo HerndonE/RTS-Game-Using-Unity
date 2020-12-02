b78484b7dde3228d70bc3da67ee817595196c988
Turtleninisoup <60590189+Turtleninisoup@users.noreply.github.com>
2020-12-02 03:39:21 -0800

    Backups.

---
+++ Unity/RTS-Game/Assets/Jaclyn/Borrowed/Scripts/BulletB.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletB : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        myRigidbody.velocity = Vector3.up * speed;
+        //Debug.Log("Wwweeeeee");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
