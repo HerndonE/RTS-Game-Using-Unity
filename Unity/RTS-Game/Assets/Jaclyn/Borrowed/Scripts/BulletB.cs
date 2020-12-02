2dd62b4422a813e93ef99e5e56a04618c21bccc6
Turtleninisoup <60590189+Turtleninisoup@users.noreply.github.com>
2020-12-02 13:32:42 -0800

    Tentative implementation of shoot sound effect.

---
+++ Unity/RTS-Game/Assets/Jaclyn/Borrowed/Scripts/BulletB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletB : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    public float speed = 5;
+    public AudioSource pewpew;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        Fire();
+        
+    }
+
+    private void Fire()
+    {
+        myRigidbody.velocity = Vector3.up * speed;
+        pewpew.Play();
+        //Debug.Log("Wwweeeeee");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
