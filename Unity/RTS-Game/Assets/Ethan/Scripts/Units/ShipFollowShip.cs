d9a9337addac6a9098f3761c0393b45cf780801d
HerndonE <EHerndon17@gmail.com>
2020-12-07 11:49:22 -0800

    [update] continuing scav func

---
+++ Unity/RTS-Game/Assets/Ethan/Scripts/Units/ShipFollowShip.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipFollowShip : MonoBehaviour {
+
+	public Transform target; 
+	public int moveSpeed;
+	public int rotationSpeed;
+	public int maxdistance;
+	private Transform myTransform;
+
+
+	public float delay = 1.0f;
+	public Transform Ammo;
+	public Transform shootPoint;
+	//public string floorTag = "Floor"; 
+
+
+	void Awake()
+	{
+		myTransform = transform;
+		myTransform.LookAt (target);
+
+	}
+	
+	
+	void Start ()
+	{
+		
+		maxdistance = 10;
+	}
+	
+	
+	void Update ()
+	{
+		maxdistance = 10;
+		myTransform.LookAt (target);
+		myTransform = transform;
+		if (Vector3.Distance (target.position, myTransform.position) > maxdistance) {
+			
+			// Get a direction vector from us to the target
+			Vector3 dir = target.position - myTransform.position;
+			
+			// Normalize it so that it's a unit direction vector
+			dir.Normalize ();
+			
+			// Move ourselves in that direction
+			myTransform.position += dir * moveSpeed * Time.deltaTime;
+
+		
+		}
+
+		if (Vector3.Distance (target.position, myTransform.position) < maxdistance) {
+			StartCoroutine(Build(delay));
+		}
+	
+
+
+}
+	IEnumerator Build(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		Instantiate(Ammo,shootPoint.position,shootPoint.rotation); 
+	}
+
+}
--- Unity/RTS-Game/Assets/Ethan/Scripts/Units/resourceCollection.cs
+++ Unity/RTS-Game/Assets/Ethan/Scripts/Units/resourceCollection.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class resourceCollection : MonoBehaviour
 {
+
+    public float resourceGatheringTime = 5.0f;
+    private bool gatheringInProduction = false;
+    public Transform target;
+    public float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,24 @@ public class resourceCollection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (this.GetComponent<NomadClick>().enabled == false) {
+            Collider m_Collider = GetComponent<Collider>();
+             m_Collider.enabled = !m_Collider.enabled;
+            resourceGatheringTime -= Time.deltaTime;
+            Debug.Log("Time Left:" + Mathf.Round(resourceGatheringTime));
+            if (resourceGatheringTime < 0)
+            {
+                Debug.Log("Resource gathered");
+                this.GetComponent<NomadClick>().enabled = true;
+                gatheringInProduction = false;
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+                //Todo Gather Resources and GoTo building and repeat
+            }
+        }
+
+
     }
 
     void OnCollisionEnter(Collision col)
@@ -21,14 +44,24 @@ public class resourceCollection : MonoBehaviour
         if (col.gameObject.tag == "mineral")
         {
             Debug.Log("I am at minerals");
-            this.GetComponent<NomadClick>().enabled = false;
-            //Debug.Log("Target Position: " + targetPosition);
+            gatheringInProduction = true;
+            if (gatheringInProduction)
+            {
+                this.GetComponent<NomadClick>().enabled = false;
+            }
+        }
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "mineral")
+        {
+            Debug.Log("I left minerals");
+            this.GetComponent<NomadClick>().enabled = true;
 
         }
-        else
-        {
-            this.GetComponent<NomadClick>().enabled = true;
-        }
+
     }
 
 }
