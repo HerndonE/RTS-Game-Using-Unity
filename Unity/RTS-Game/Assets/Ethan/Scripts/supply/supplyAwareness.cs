66ff6e6e9d97a9059bb083b490694a22d0d88c4e
HerndonE <EHerndon17@gmail.com>
2020-12-08 00:05:45 -0800

    [finished] resource functionality!

---
+++ Unity/RTS-Game/Assets/Ethan/Scripts/supply/supplyAwareness.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class supplyAwareness : MonoBehaviour
+{
+
+    public GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (GameObject.FindGameObjectWithTag("scavenger") != null)
+        {
+            //it exists
+            mainCamera.GetComponent<buildSupply>().enabled = false;
+        }
+        else
+        {
+            mainCamera.GetComponent<buildSupply>().enabled = true;
+        }
+
+    }
+}
